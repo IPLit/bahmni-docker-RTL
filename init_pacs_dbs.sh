73d6cd28a02b0eaa4c8b1bd9e46c221411e16272
Akhil Anto Tharayil <atharayil@iplit.in>
2026-01-13 15:27:51 +0530

    Updated tags and PACS fix

--- init_pacs_dbs.sh
+++ init_pacs_dbs.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 set -e
 
+export PGPASSWORD="${POSTGRES_PASSWORD}"
+
 create_user_and_database() {
-  export PG_PASSWORD=${POSTGRES_PASSWORD}
   DB_NAME="$1"
   DB_USERNAME="$2"
   DB_PASSWORD="$3"
-  psql -c "CREATE DATABASE ${DB_NAME};"
-  psql -c "REVOKE ALL ON DATABASE ${DB_NAME} FROM PUBLIC;"
-  psql -c "CREATE USER ${DB_USERNAME} WITH ENCRYPTED PASSWORD '${DB_PASSWORD}';"
-  psql -c "GRANT ALL ON DATABASE ${DB_NAME} TO ${DB_USERNAME};"
+
+  echo "Ensuring database ${DB_NAME} exists"
+  psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname='${DB_NAME}'" | grep -q 1 \
+    || psql -U postgres -c "CREATE DATABASE ${DB_NAME};"
+
+  echo "Ensuring user ${DB_USERNAME} exists"
+  psql -U postgres -tc "SELECT 1 FROM pg_roles WHERE rolname='${DB_USERNAME}'" | grep -q 1 \
+    || psql -U postgres -c "CREATE USER ${DB_USERNAME};"
+
+  echo "Setting password for ${DB_USERNAME}"
+  psql -U postgres -c "ALTER USER ${DB_USERNAME} WITH ENCRYPTED PASSWORD '${DB_PASSWORD}';"
+
+  echo "Granting privileges on ${DB_NAME} to ${DB_USERNAME}"
+  psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${DB_NAME} TO ${DB_USERNAME};"
 }
 
 create_user_and_database "${DCM4CHEE_DB_NAME}" "${DCM4CHEE_DB_USERNAME}" "${DCM4CHEE_DB_PASSWORD}"
